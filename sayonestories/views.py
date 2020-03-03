a132da8fd8c49252ca1ac1b448c6afa114740f4a
sarathck96 <59605470+sarathck96@users.noreply.github.com>
2020-03-03 14:26:32 +0530

    Update views.py

--- sayonestories/views.py
+++ sayonestories/views.py
@@ -85,7 +85,7 @@ def validate_register(request):
 
     return render(request, 'sayonestories/userregistration.html', {'form': form})
 
-
+@login_required(login_url='/accounts/login/')
 def User_home_page(request):
     """loads the  Home page for specific user"""
 
@@ -112,7 +112,7 @@ def User_home_page(request):
 
     return render(request, 'sayonestories/UserHome.html', context={'img_url': pic_url, 'stories': users})
 
-
+@login_required(login_url='/accounts/login/')
 def add_story_page(request):
     """loads the page containing form to add story """
 
@@ -120,7 +120,7 @@ def add_story_page(request):
 
     return render(request, 'sayonestories/addstory.html', context={'form': form})
 
-
+@login_required(login_url='/accounts/login/')
 def add_story(request):
     """ adds the story enterd by the user to the database """
     if request.method == 'POST':
@@ -139,7 +139,7 @@ def add_story(request):
         
         render(request, 'sayonestories/addstory.html', context={'form': form})
 
-
+@login_required(login_url='/accounts/login/')
 def add_sub_story(request):
     """ substory is the story type (it could be Event,Blog,image gallery) . user is shown different forms depending on
      the story type . this view adds the substory to the database """
@@ -153,7 +153,7 @@ def add_sub_story(request):
         form2 = MultiUploadForm()
         return render(request, 'sayonestories/addstory.html', context={'story': story_object, 'form2': form2})
 
-
+@login_required(login_url='/accounts/login/')
 def add_blog(request):
     """ if the story type is blog or event then this view adds the blog/event to the database """
     print('call here')
@@ -173,13 +173,13 @@ def add_blog(request):
     else:
         return render(request, 'sayonestories/addstory.html', context={})
 
-
+@login_required(login_url='/accounts/login/')
 def user_stories_page(request):
     # loads the page containing all stories added by the specific user
     story_objects_for_user = Story.objects.filter(story_user=request.user).filter(story_status=1)
     return render(request, 'sayonestories/userstories_page.html', context={'stories': story_objects_for_user})
 
-
+@login_required(login_url='/accounts/login/')
 def story_detail_page(request, id):
     """loads all the details regarding the selected story .details include story title,author,date created and
     substory details .if substory is blog or event the details include title,image and description .if substory is
@@ -213,7 +213,7 @@ def story_detail_page(request, id):
                     'comments': comments}
         return render(request, 'sayonestories/story_detail_page.html', context=context1)
 
-
+@login_required(login_url='/accounts/login/')
 def delete_story(request, story_id):
     """ deletes the selected story and generate alert box to tell user that story has been deleted """
     item = Story.objects.get(story_id=story_id)
@@ -221,7 +221,7 @@ def delete_story(request, story_id):
     messages.success(request, ("Story has been deleted"))
     return redirect(user_stories_page)
 
-
+@login_required(login_url='/accounts/login/')
 def user_profile_page(request):
     """ shows the details of the user's profile .details include name,username,email ,profile pic and number of
     stories added by user. """
@@ -241,7 +241,7 @@ def user_profile_page(request):
     print('kkk', profile_details)
     return render(request, 'sayonestories/UserProfile.html', context={'profile_details': profile_details})
 
-
+@login_required(login_url='/accounts/login/')
 def update_profile_pic(request):
     """ allows the user to update the profile picture """
 
@@ -269,7 +269,7 @@ def like_story(request, story_id):
     data = {'is_valid': can_like}
     return JsonResponse(data)
 
-
+@login_required(login_url='/accounts/login/')
 def add_multiple_pics(request, story_id):
     """ This view helps the user to load image gallery as part of his/her story . story_id is the unique id of the story
     that has been passed . story_id is used to fetch story object and map story and images in the database """
@@ -369,6 +369,7 @@ def test(request):
     test_obj = Story.objects.get(story_id=3)
     print('ttt',test_obj.image_story.all()[0].file)
 
+@login_required(login_url='/accounts/login/')
 def edit_profile_page(request):
 
     profile_details = {}
@@ -385,7 +386,7 @@ def edit_profile_page(request):
                      
 
 
-
+@login_required(login_url='/accounts/login/')
 def story_detail_page2(request,id):
     story_obj = Story.objects.filter(story_id=id)[0]
     comments = Comments.objects.filter(story_commented=story_obj)
@@ -404,20 +405,25 @@ def story_detail_page2(request,id):
         context1 = {'substory': sub_story_object, 'story': story_obj,'comments': comments}
         return render(request, 'sayonestories/story_detail_page2.html', context=context1)
 
-
+@login_required(login_url='/accounts/login/')
 def show_events(request):
     all_event_objects = Story.objects.all().order_by('-date_created').filter(story_status=1).filter(story_type=0)
     return render(request,'sayonestories/events_page.html',context={'stories':all_event_objects})
 
+
+@login_required(login_url='/accounts/login/')
 def show_blogs(request):
     all_blog_objects = Story.objects.all().order_by('-date_created').filter(story_status=1).filter(story_type=1)
     print('blogs',all_blog_objects)
     return render(request,'sayonestories/blogs_page.html',context={'stories':all_blog_objects})
 
+@login_required(login_url='/accounts/login/')
 def show_gallery(request):
     all_gallery_objects = Story.objects.all().order_by('-date_created').filter(story_status=1).filter(story_type=2)
     return render(request,'sayonestories/image_gallery_page.html',context={'stories':all_gallery_objects})
 
+
+@login_required(login_url='/accounts/login/')
 def top_authors(request):
     test = Story.objects.values('story_user_id').annotate(story_user_count=Count('story_user')).order_by('-story_user_count')[:2]
     top_users = []
@@ -442,7 +448,7 @@ def top_authors(request):
         
     return render(request,'sayonestories/top_authors.html',context={'authors':top_users})
             
-
+@login_required(login_url='/accounts/login/')
 def edit_story_page(request, id):
    story_obj = Story.objects.get(story_id=id)
    story_type = story_obj.story_type
@@ -462,7 +468,7 @@ def edit_story_page(request, id):
        return render(request, 'sayonestories/story_edit_page.html', context)
 
 
-
+@login_required(login_url='/accounts/login/')
 def edit_story(request):
    story_id = request.POST.get('id')
    print('id',story_id)
@@ -490,7 +496,7 @@ def edit_story(request):
    else:
         return redirect('story_detail_page', id=story_id)
 
-
+@login_required(login_url='/accounts/login/')
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
@@ -507,7 +513,7 @@ def change_password(request):
         'form': form
     })
 
-
+@login_required(login_url='/accounts/login/')
 def top_stories_page(request):
 
     top_events = Story.objects.filter(story_type=0).order_by('-story_likes')[:3]
@@ -516,3 +522,254 @@ def top_stories_page(request):
 
     context = {'events':top_events,'blogs':top_blogs,'gallery':top_gallery}
     return render(request, 'sayonestories/top_stories_page.html',context )
+
+
+@login_required(login_url='/accounts/login/')
+def filter(request):
+    param = request.POST.get('filterparam')
+    print('param',param)
+
+
+
+    results = Story.objects.filter(Q(story_title__icontains=param))
+    context = {'stories': results}
+    if len(results) == 0:
+        results = Blog.objects.filter(Q(blog_description__icontains=param))
+        results2 = ''
+        for item in results:
+            results2 = Story.objects.filter(story_title=item)
+
+        context = {'stories': results2}
+
+
+
+
+    return render(request, 'sayonestories/filtered_results.html', context)
+
+@login_required(login_url='/accounts/login/')
+def all_stories_page(request):
+
+
+    pic_url = request.user.sayone_user.profile_pic
+
+    all_story_objects = Story.objects.all().order_by('-date_created').filter(story_status=1)
+
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(all_story_objects, 6)
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
+    return render(request, 'sayonestories/allstories.html', context={'img_url': pic_url, 'stories': users})
+
+@login_required(login_url='/accounts/login/')
+def updategallery(request,story_id):
+
+
+    story_id = story_id
+    story_obj = Story.objects.filter(story_id=story_id)[0]
+    if request.method == 'POST':
+        form2 = MultiUploadForm2()
+
+        form = MultiUploadForm2(request.POST, request.FILES)
+        if form.is_valid():
+            for each in form.cleaned_data['file']:
+                print('lll', each)
+                Images.objects.create(file=each, story=story_obj)
+        context = {'title': story_obj.story_title, 'id': story_obj.story_id,'story':story_obj,'form':form2}
+        return render(request,'sayonestories/story_edit_page.html',context)
+    else:
+        context = {'title': story_obj.story_title, 'id': story_obj.story_id,'story':story_obj,'form':form}
+        return render(request, 'sayonestories/story_edit_page.html', context)
+
+@login_required(login_url='/accounts/login/')
+def record_view(request,story_id):
+    story_obj = get_object_or_404(Story, pk=story_id)
+
+    if not StoryView.objects.filter(story=story_obj,session=request.session.session_key):
+        view = StoryView.objects.create(story=story_obj,session=request.session.session_key)
+        view.save()
+
+    number_of_visits = StoryView.objects.filter(story=story_obj).count()
+
+    return number_of_visits
+
+
+
+
+
+
+
+
+def user_email_login(request):
+    flag = ''
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+
+    if re.search(regex, username):
+        flag = 'mail'
+    else:
+        flag = 'username'
+
+    if flag == 'mail':
+        queryset = User.objects.filter(email=username)[0]
+        user_name = queryset.username
+        user = auth.authenticate(username=user_name, password=password)
+
+        if user is not None:
+            if user.is_active:
+                auth.login(request, user)
+            return redirect('/verifypage')
+        else:
+            messages.success(request, 'email or password is incorrect')
+            return redirect('/accountss/login/')
+    else:
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                auth.login(request, user)
+            return redirect('/verifypage')
+        else:
+            messages.success(request, 'username or password is incorrect')
+            return redirect('/accountss/login/')
+
+
+def verify_user_page(request):
+    print('call here')
+    if request.user.sayone_user.verified == True:
+        return redirect('/userhome')
+    else:
+        otp = random.randint(10000, 90000)
+        user_obj = Sayoneuser.objects.filter(user=request.user).update(otp=otp)
+
+        email_body = f"""
+        Hi {request.user.username},
+
+        use this code {str(otp)} to verify your Sayonestories account
+
+        Thanks for registering with us
+
+        Sincerely,
+        Sayonestories Team
+        """
+
+
+
+        send_mail(
+            'Account verification Sayonestories',
+            email_body,
+            'testusersayone@gmail.com',
+            [request.user.email],
+            fail_silently=False,
+        )
+    return render(request, 'sayonestories/verify_account.html', context={})
+
+
+def verify_final(request):
+    entered_otp = request.POST.get('otp')
+    print('lll', entered_otp)
+    otp2 = request.user.sayone_user.otp
+
+    if str(entered_otp) == str(otp2):
+        user_obj = Sayoneuser.objects.filter(user=request.user)[0]
+        user_obj.verified = True
+        user_obj.save()
+        return redirect('/userhome')
+    else:
+        return render(request, 'sayonestories/verify_account.html',
+                      context={'error': 'Please check your verification code'})
+
+
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def add_reply(request):
+    print('call here...........///////////')
+
+    if request.POST.get('action') == 'post':
+        story_id = request.POST.get('story_id')
+        comm_id = request.POST.get('comment_id')
+        reply_text = request.POST.get('reply_text')
+        story_id2 = request.POST.get('story_id2')
+    print('id2', story_id2)
+    if story_id2 is None:
+        print('reply section')
+        story_obj = Story.objects.filter(story_id=story_id)[0]
+        print('storyid', story_id)
+        print('commentid', comm_id)
+        print('replytext', reply_text)
+
+        comment_obj = Comments.objects.filter(id=comm_id)[0]
+        user_replied = request.user
+        user_replied_to = comment_obj.user_commented
+
+        reply_obj = Replies.objects.create(reply=reply_text, comment_replied=comment_obj, user_replied=user_replied,
+                                           user_replied_to=user_replied_to)
+
+        response_list = []
+        for item in comment_obj.reply_to_comment.all():
+            temp_dict = {}
+            temp_dict['reply'] = item.reply
+            temp_dict['replied_by'] = item.user_replied.get_full_name()
+            temp_dict['replied_to'] = item.user_replied_to.get_full_name()
+
+        print(temp_dict)
+        return JsonResponse({'response': temp_dict, 'divid': comm_id}, safe=False)
+    else:
+        print('comment section')
+        story_obj = Story.objects.filter(story_id=story_id2)[0]
+        user_commented = request.user
+        comment = request.POST.get('comment')
+        comment_obj = Comments.objects.create(user_commented=user_commented, story_commented=story_obj, comment=comment)
+
+
+
+        temp_dict2 = {}
+        temp_dict2['comment'] = comment_obj.comment
+        temp_dict2['user_commented'] = comment_obj.user_commented.sayone_user.name
+
+        temp_dict2['comment_id'] = comment_obj.id
+        temp_dict2['story_id'] = story_id2
+
+        temp_dict2['date_commented']=comment_obj.date_commented
+
+        return JsonResponse({'response': temp_dict2, 'comment': 'com'}, safe=False)
+    
+    
+@login_required(login_url='/accounts/login/')
+def add_to_fav(request,story_id):
+
+    print('call in fav')
+    story_obj = Story.objects.get(story_id=story_id)
+    fav_obj = Favourites.objects.filter(user=request.user,story_added=story_obj)
+
+    if len(fav_obj) == 0:
+        new_fav_obj = Favourites.objects.create(user=request.user, story_added=story_obj)
+        return JsonResponse({'added':'yes'})
+
+    else:
+
+        return JsonResponse({'added':'no'})
+    
+    
+@login_required(login_url='/accounts/login/')
+def my_favourites(request):
+
+    fav_list =Favourites.objects.filter(user=request.user)
+    story_list = []
+    for item in fav_list:
+        Story_obj =Story.objects.filter(story_title=item.story_added)[0]
+        story_list.append(Story_obj)
+    return render(request,'sayonestories/favourites_page.html',context={'stories':story_list})
+
+
